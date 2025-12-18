651e27f066d0552595a406a046b7711085f05ce8
Snoxey <GenDashen@gmail.com>
2025-12-19 00:15:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ document.getElementById("year").textContent = new Date().getFullYear();
 // --------------------
 // Gallery from GitHub /assets folder
 // --------------------
-const GITHUB_OWNER = "Snoxxey";     // change if needed
+const GITHUB_OWNER = "SnoxXEY";     // change if needed
 const GITHUB_REPO  = "portfolio";  // change if needed
 const ASSETS_PATH  = "assets";
 
@@ -150,4 +150,5 @@ async function loadGalleryFromGitHub() {
 }
 
 loadGalleryFromGitHub();
+
 
